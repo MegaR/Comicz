6a989f60407edfc489017a18427261dbd468c7bd
RAalders <raalders@boomerweb.nl>
2018-04-04 15:45:03 +0200

    actually expire cache

--- server/cache.js
+++ server/cache.js
@@ -1,10 +1,19 @@
 class Cache {
-    constructor() {
+    constructor(timeoutTime = 1000*60*60*12) {
+        this.timeoutTime = timeoutTime;
         this.cache = {};
+        this.timeouts = {};
     }
 
     store(url, data) {
+        clearTimeout(this.timeouts[url]);
+
         this.cache[url] = data;
+
+        this.timeouts[url] = setTimeout(()=>{
+            delete this.cache[url];
+            delete this.timeouts[url];
+        }, this.timeoutTime);
     }
 
     get(url) {
