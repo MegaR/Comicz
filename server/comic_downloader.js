ff3a21e902839427ded51b22de8702ecdf1f24ee
RAalders <raalders@boomerweb.nl>
2018-05-13 19:14:26 +0200

    cache comics results server side

--- server/comic_downloader.js
+++ server/comic_downloader.js
@@ -1,4 +1,5 @@
 const storage = require('./storage.js');
+const Cache = require('./cache.js');
 
 const comicSources = [
     require('./comic_sources/readcomicsonline_ru.js'),
@@ -8,6 +9,11 @@ const comicSources = [
 
 
 class ComicDownloader {
+
+    constructor() {
+        this.cache = new Cache();
+    }
+
     setup(app) {
         app.post('/downloader/search', (req, res) => {
             this.search(req.body)
@@ -46,6 +52,9 @@ class ComicDownloader {
     }
 
     async search(data) {
+        const cacheResult = this.cache.get('search/'+JSON.stringify(data));
+        if(cacheResult) return cacheResult;
+
         let results = [];
         for(let source of comicSources) {
             try {
@@ -55,19 +64,28 @@ class ComicDownloader {
             }
         }
 
+        this.cache.store('search/'+JSON.stringify(data), results);
         return results;
     }
 
     async page(issueId, sourceName, volumeName, issueNr, pageNr) {
+        const cachePage = this.cache.get(`page/${issueId}/${sourceName}/${volumeName}/${issueNr}/${pageNr}`);
+        if(cachePage) return cachePage;
+
         const source = comicSources.filter(source=>source.name === sourceName)[0];
         if(!source) {
             throw new Error("Unknown comic source");
         }
         let page = await source.page(volumeName, issueNr, Number(pageNr));
+
+        this.cache.store(`page/${issueId}/${sourceName}/${volumeName}/${issueNr}/${pageNr}`, page);
         return page;
     }
 
     async details(issueId, sourceName, volumeName, issueNr) {
+        const cacheDetails = this.cache.get(`details/${issueId}/${sourceName}/${volumeName}/${issueNr}`);
+        if(cacheDetails) return cacheDetails;
+
         const source = comicSources.filter(source=>source.name === sourceName)[0];
         if(!source) {
             throw new Error("Unknown comic source");
@@ -76,6 +94,7 @@ class ComicDownloader {
         let details = await source.details(volumeName, issueNr);
         details.progress = Number(await storage.getProgress(issueId));
 
+        this.cache.store(`details/${issueId}/${sourceName}/${volumeName}/${issueNr}`, details);
         return details;
     }
 }
