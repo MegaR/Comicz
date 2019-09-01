50523ec763606bb7a5fb12ad29874a81a36e71ab
RAalders <raalders@boomerweb.nl>
2019-09-01 15:44:56 +0200

    config setup

---
+++ src/config/config.inferface.ts
@@ -0,0 +1,5 @@
+export default interface Config {
+    database: {
+        test: string
+    }
+}
---
+++ src/config/config.service.ts
@@ -0,0 +1,24 @@
+import {join} from 'path';
+import {existsSync} from 'fs';
+import * as defaultConfig from './config.default.json';
+import Config from './config.inferface';
+import {Logger} from '@nestjs/common';
+
+export class ConfigService {
+    private readonly config: Config;
+    private readonly logger: Logger = new Logger('ConfigService');
+
+    constructor() {
+        const location = join(process.cwd(), 'config.json');
+        if (existsSync(location)) {
+            this.config = <Config> {...defaultConfig, ...require(location)};
+        } else {
+            this.logger.warn('No custom config found. Using default only.');
+            this.config = <Config> defaultConfig;
+        }
+    }
+
+    get(): Config {
+        return this.config;
+    }
+}
